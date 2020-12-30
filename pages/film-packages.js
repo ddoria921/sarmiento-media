7979ce796635e8af522686317d056a1d23289319
Darin Doria <ddoria@wistia.com>
2020-12-29 23:03:26 -0500

    Remove ImageGallery from film-packages page

--- pages/film-packages.js
+++ pages/film-packages.js
@@ -1,10 +1,7 @@
-import ImageGallery from "../components/image-gallery";
-
 export default function FilmPackages() {
   return (
     <div>
       <h1>Film Packages</h1>
-      <ImageGallery></ImageGallery>
     </div>
   );
 }
