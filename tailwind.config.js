ce5daa4f5a25a1d7967fb822911db12045d7dd33
Darin Doria <ddoria@wistia.com>
2021-01-02 01:28:19 -0500

    Update tailwind config with font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./components/**/*.js", "./pages/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cursive: ["High Tide"],
+      },
+    },
   },
   variants: {
     extend: {},
