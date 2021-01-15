61c9ad9080c40e3a2ca4e4be1dec9b5ef7d0735e
Darin Doria <ddoria@wistia.com>
2021-01-14 21:53:51 -0500

    Support showing multiple instagram media types

--- components/instagram-gallery.js
+++ components/instagram-gallery.js
@@ -1,3 +1,10 @@
+import { useEffect } from "react";
+import { getInstagramImages } from "../lib/api";
+
+/**
+ *
+ * @param {Object} props.images - object containing imageUrl, id & permalink
+ */
 export default function InstagramGallery({ className, images }) {
   return (
     <div className={`w-full ${className}`.trim()}>
@@ -25,15 +32,46 @@ export default function InstagramGallery({ className, images }) {
       </a>
       <div className="grid grid-cols-4 gap-2 mt-6">
         {images &&
-          images.slice(0, 8).map((imageUrl, i) => {
+          images.slice(0, 8).map(({ id, imageUrl, permalink, type }, i) => {
             return (
-              <div className="" key={i}>
-                <img className="w-full h-full object-cover" src={imageUrl} />
-              </div>
+              <a
+                href={permalink}
+                key={id}
+                className="relative"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="absolute inset-0 z-10 group flex justify-center">
+                  <div className="absolute inset-0 bg-black opacity-0 z-0 transition ease-in duration-200 group-hover:opacity-50"></div>
+                  <div className="w-6 h-6 z-10 text-gray-100 self-center opacity-0 transition ease-in duration-200 group-hover:opacity-100">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                      <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                    </svg>
+                  </div>
+                </div>
+                <div className="aspect-w-1 aspect-h-1">
+                  <img className="w-full h-full object-cover" src={imageUrl} />
+                </div>
+                <InstagramMediaLabel type={type} />
+              </a>
             );
           })}
       </div>
     </div>
+  );
+}
+
+function InstagramMediaLabel({ type = "" }) {
+  const labelType = type.toLowerCase().replace("_", "-");
+  return (
+    <span
+      className={`media-label ${labelType} block absolute top-0 right-0`}
+    ></span>
   );
 }
 
